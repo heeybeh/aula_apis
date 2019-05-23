e19d079155c88c9333def1a742c140e4c040a86e
beatrice.fernandes@realizecfi.com.br <beatrice.fernandes@realizecfi.com.br>
2019-05-22 23:35:23 -0300

    controller and model

---
+++ contactModel.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+var Contact = module.exports = mongoose.model('contact', contactSchema);
+module.exports.get = function (callback, limit) {
+    Contact.find(callback).limit(limit);
+}
